9ea121b5cb0351236331ed558f2081eec3d1f94b
Moch. Rofiqi <mrfiqi323@gmail.com>
2022-10-08 07:25:59 +0700

    Catatan React

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Swal from 'sweetalert2';
 import withReactContent from 'sweetalert2-react-content';
+import PropsTypes from 'prop-types';
 
 function Alert({ title, message }){
     const Pesan = withReactContent(Swal);
@@ -9,6 +10,11 @@ function Alert({ title, message }){
         html: <p>{message}</p>,
         icon: 'success',
     })
+}
+
+Alert.propsTypes = {
+    title: PropsTypes.string.isRequired,
+    message: PropsTypes.string.isRequired,
 }
 
 export default Alert;
